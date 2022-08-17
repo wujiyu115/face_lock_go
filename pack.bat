4a743c5ed8b401ef1dd76bd059fa61889ccea3fe
wujiyu115 <wujiyu115@gmail.com>
2022-08-17 13:31:03 +0800

    添加upx压缩生成包

--- pack.bat
+++ pack.bat
@@ -5,18 +5,13 @@ Color 0A
 REM 声明采用UTF-8编码
 chcp 65001
 
-
-:: 2.rsrc
-:2
-echo "需要先安装工具命令rsrc 命令: go get github.com/akavel/rsrc"
-
 echo rsrc manifesting...
-rsrc -arch amd64 -manifest facelock.exe.manifest -o facelock.exe.syso -ico icon.ico
+start tools/rsrc.exe -arch amd64 -manifest tools/facelock.exe.manifest -o facelock.exe.syso -ico icon.ico
 
 echo building...
 go build -o facelock.exe -ldflags="-w -s  -H windowsgui"
-goto end
 
-:end
+echo upx.....
+start tools/upx.exe facelock.exe
 echo complete
 pause
